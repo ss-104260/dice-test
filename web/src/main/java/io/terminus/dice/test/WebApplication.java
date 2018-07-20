0931e71e3603e94e82495406b2421e02bcc82a67
appsoner <appsoner@gmail.com>
2018-07-20 17:00:46 +0800

    add addon

--- web/src/main/java/io/terminus/dice/test/WebApplication.java
+++ web/src/main/java/io/terminus/dice/test/WebApplication.java
@@ -3,10 +3,13 @@ package io.terminus.dice.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(WebApplication.class);
         application.run(args);
     }
+
 }
